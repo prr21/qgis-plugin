79ecb1c87429679d01664d787e590e9a61e8236f
Kurban <Mr.gustav009@gmail.com>
2020-06-16 04:17:30 +0300

    Изменения пути

--- __init__.py
+++ __init__.py
@@ -31,6 +31,6 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    #
     from .ncfu_plugin import NcfuPlugin
+
     return NcfuPlugin(iface)
